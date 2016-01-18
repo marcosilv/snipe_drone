4bf83cd98850d247bf067a32de3617f5e1a86ea0
marcosilv <marco_0094@hotmail.com>
2016-01-18 18:46:35 +0100

    Added ardrone_helpers

---
+++ src/ardrone_helpers/ardrone_joystick/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/marco/catkin_ws4/src/ardrone_helpers/ardrone_joystick/build/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/marco/catkin_ws4/src/ardrone_helpers/ardrone_joystick/build/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/marco/catkin_ws4/src/ardrone_helpers/ardrone_joystick/build/devel/lib:/home/marco/catkin_ws4/src/ardrone_helpers/ardrone_joystick/build/devel/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
+export PATH="/home/marco/catkin_ws4/src/ardrone_helpers/ardrone_joystick/build/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/marco/catkin_ws4/src/ardrone_helpers/ardrone_joystick/build/devel/lib/pkgconfig:/home/marco/catkin_ws4/src/ardrone_helpers/ardrone_joystick/build/devel/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH"
+export PYTHONPATH="/home/marco/catkin_ws4/src/ardrone_helpers/ardrone_joystick/build/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/marco/catkin_ws4/src/ardrone_helpers/ardrone_joystick/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/marco/catkin_ws4/src/ardrone_helpers/ardrone_joystick:$ROS_PACKAGE_PATH"
